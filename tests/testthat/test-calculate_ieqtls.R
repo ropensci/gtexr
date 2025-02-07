4ff53cbc60d7ba00cc3f1f471ac777405fd5ed2c
rmgpanw <rmgpanw@ucl.ac.uk>
2025-02-07 23:29:44 +0000

    update test-calculate_ieqtls.R

--- tests/testthat/test-calculate_ieqtls.R
+++ tests/testthat/test-calculate_ieqtls.R
@@ -1,5 +1,75 @@
 test_that("`calculate_ieqtls()` returns tibble with expected colnames", {
+
+  mocked_gtex_query <- function(...) {
+    list(
+      cellType = "Adipocytes",
+      data = list(0.2038),
+      datasetId = "gtex_v8",
+      enrichmentScores = list(0.6664),
+      gencodeId = "ENSG00000203782.5",
+      genotypes = list(0),
+      regressionCoord = list(
+        `0` = list(
+          -3.042728,
+          3.042728,-0.0232111413501241,-0.00245020517878138
+        ),
+        `1` = list(-3.042728, 3.042728, 0.21073213891777, -0.157052171868657),
+        `2` = list(-3.042728, 3.042728, 0.444675419185664, -0.311654138558532)
+      ),
+      tissueSiteDetailId = "Adipose_Subcutaneous",
+      variantId = "chr1_1099341_T_C_b38"
+    )
+  }
+
+  testthat::with_mocked_bindings(
+    code = {
+      ieqtls <- calculate_ieqtls(
+        cellType = "Adipocytes",
+        tissueSiteDetailId = "Adipose_Subcutaneous",
+        gencodeId = "ENSG00000203782.5",
+        variantId = "chr1_1099341_T_C_b38"
+      )
+    },
+    gtex_query = mocked_gtex_query
+  )
+
+  expect_identical(
+    names(ieqtls),
+    c(
+      "cellType",
+      "data",
+      "datasetId",
+      "enrichmentScores",
+      "gencodeId",
+      "genotypes",
+      "regressionCoord",
+      "tissueSiteDetailId",
+      "variantId"
+    )
+  )
+
+  expect_identical(
+    ieqtls$data[[1]],
+    0.2038
+  )
+
+  expect_identical(
+    ieqtls$enrichmentScores[[1]],
+    0.6664
+  )
+
+  expect_identical(
+    ieqtls$genotypes[[1]],
+    0
+  )
+
+  expect_identical(
+    names(ieqtls$regressionCoord[[1]]),
+    as.character(0:2)
+  )
+
   skip_if_offline()
+
   result <-
     calculate_ieqtls(
       cellType = "Adipocytes",
