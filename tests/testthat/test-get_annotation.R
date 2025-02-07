4ae9c76ee04978e647041a89cf7b06f98e2a681f
rmgpanw <rmgpanw@ucl.ac.uk>
2025-02-07 23:18:52 +0000

    udpate test-get_annotation.R

--- tests/testthat/test-get_annotation.R
+++ tests/testthat/test-get_annotation.R
@@ -1,4 +1,51 @@
 test_that("`get_annotation()` returns tibble with expected colnames", {
+
+  mocked_gtex_query <- function(...) {
+    c(list(data = list(
+      list(
+        entity = "subject",
+        values = list("male", "female"),
+        type = "enum",
+        annotation = "sex",
+        datasetId = "gtex_v8"
+      ),
+      list(
+        entity = "subject",
+        values = list("20-29", "30-39", "40-49", "50-59", "60-69", "70-79"),
+        type = "enum",
+        annotation = "ageBracket",
+        datasetId = "gtex_v8"
+      )
+    )), list(
+      paging_info = list(
+        numberOfPages = 1,
+        page = 0,
+        maxItemsPerPage = 250,
+        totalNumberOfItems = 2
+      )
+    ))
+  }
+
+  testthat::with_mocked_bindings(
+    code = {
+      annotation <- get_annotation()
+    },
+    gtex_query = mocked_gtex_query
+  )
+
+  expect_identical(
+    names(annotation),
+    c("entity", "values", "type", "annotation", "datasetId")
+  )
+
+  expect_identical(
+    annotation$values[[1]],
+    c("male", "female")
+  )
+
+  expect_identical(annotation$values[[2]],
+                   c("20-29", "30-39", "40-49", "50-59", "60-69", "70-79"))
+
   skip_if_offline()
   result <- get_annotation()
 
