18dc26d71fe2ed04b35b2f007885d188c962cc01
rmgpanw <rmgpanw@ucl.ac.uk>
2025-02-07 23:08:06 +0000

    update test-get_single_nucleus_gex.R

--- tests/testthat/test-get_single_nucleus_gex.R
+++ tests/testthat/test-get_single_nucleus_gex.R
@@ -1,4 +1,60 @@
 test_that("`get_single_nucleus_gex()` returns tibble with expected colnames", {
+
+  mocked_gtex_query <- function(...) {
+    list(
+      data = list(
+        list(
+          tissueSiteDetailId = "Muscle_Skeletal",
+          ontologyId = "UBERON:0011907",
+          datasetId = "gtex_snrnaseq_pilot",
+          gencodeId = "ENSG00000203782.5",
+          geneSymbol = "LOR",
+          cellTypes = list(
+            list(
+              cellType = "Immune (DC/macrophage)",
+              count = 3,
+              meanWithZeros = 0.0100524696772936,
+              meanWithoutZeros = 2.41594354577622,
+              medianWithZeros = 0,
+              medianWithoutZeros = 2.72691867539705,
+              numZeros = 718,
+              data = NULL
+            )
+          ),
+          unit = "log(CP10K)"
+        )
+      ),
+      paging_info = list(
+        numberOfPages = 1,
+        page = 0,
+        maxItemsPerPage = 250,
+        totalNumberOfItems = 7
+      )
+    )
+  }
+
+  testthat::with_mocked_bindings(
+    code = {
+      single_nucleus_gex <- get_single_nucleus_gex(
+        gencodeIds = "ENSG00000203782.5"
+      )
+    },
+    gtex_query = mocked_gtex_query
+  )
+
+  expect_identical(
+    names(single_nucleus_gex),
+    c(
+      "tissueSiteDetailId",
+      "ontologyId",
+      "datasetId",
+      "gencodeId",
+      "geneSymbol",
+      "cellTypes",
+      "unit"
+    )
+  )
+
   skip_if_offline()
   # `excludeDataArray = FALSE`
   result <- get_single_nucleus_gex(
