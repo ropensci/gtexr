4d3bea57ec76bbc89b0516d6b1294311809da154
rmgpanw <rmgpanw@ucl.ac.uk>
2025-04-26 08:29:16 +0100

    update test-gt_sample_datasetes.R and increment dev version

--- tests/testthat/test-get_sample_datasets.R
+++ tests/testthat/test-get_sample_datasets.R
@@ -1,29 +1,34 @@
-test_that("`get_sample_datasets()` returns tibble with expected colnames", {
-  skip_if_offline()
-  result <- get_sample_datasets(itemsPerPage = 1) |>
-    suppressWarnings()
+test_that("`get_sample_datasets()` returns tibble with expected colnames",
+          {
+            skip_if_offline()
+            result <- get_sample_datasets(itemsPerPage = 10) |>
+              suppressWarnings()
 
-  expect_s3_class(result, "tbl_df")
+            expect_s3_class(result, "tbl_df")
 
-  expect_identical(
-    names(result),
-    c(
-      "ischemicTime",
-      "aliquotId",
-      "tissueSampleId",
-      "tissueSiteDetail",
-      "dataType",
-      "ischemicTimeGroup",
-      "freezeType",
-      "sampleId",
-      "sampleIdUpper",
-      "ageBracket",
-      "hardyScale",
-      "tissueSiteDetailId",
-      "subjectId",
-      "uberonId",
-      "sex",
-      "datasetId"
-    )
-  )
-})
+            expect_true(all(
+              names(result) %in%
+                c(
+                  "ischemicTime",
+                  "aliquotId",
+                  "tissueSampleId",
+                  "tissueSiteDetail",
+                  "dataType",
+                  "ischemicTimeGroup",
+                  "freezeType",
+                  "sampleId",
+                  "sampleIdUpper",
+                  "ageBracket",
+                  "hardyScale",
+                  "tissueSiteDetailId",
+                  "subjectId",
+                  "uberonId",
+                  "sex",
+                  "datasetId",
+                  "rin",
+                  "pathologyNotesCategories",
+                  "pathologyNotes",
+                  "autolysisScore"
+                )
+            ))
+          })
